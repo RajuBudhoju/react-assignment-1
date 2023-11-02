98a9e20889bf71b1177ba89658e1b330be70033e
RajuBudhoju <budhojuraju>
2023-11-02 21:23:07 +0530

    form with products

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+import ReactDOM from 'react-dom';
 
-function App() {
+
+const App = () => {
+  const [enteredTitle, setEnteredTitle] = useState('');
+  const [enteredAmount, setEnteredAmount] = useState('');
+  const [expenses, setExpenses] = useState([]);
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const expenseData = {
+        title: enteredTitle,
+        amount: enteredAmount,
+    };
+
+    setExpenses((prevExpenses) => [...prevExpenses, expenseData]);
+
+    setEnteredTitle('');
+    setEnteredAmount('');
+  };
+
+  const deleteExpenseHandler = (index) => {
+    setExpenses((prevExpenses) => prevExpenses.filter((_, i) => i !== index));
+  };
+
+  const totalAmount = expenses.reduce((total, expense) => total + parseFloat(expense.amount), 0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <form onSubmit={submitHandler}>
+        <label>Product Name: </label>
+        <input type='text' value = {enteredTitle} onChange={(event) => setEnteredTitle(event.target.value)}></input>
+
+        <label> Amount: </label>
+        <input type='number' min="0.01" step="0.01" value = {enteredAmount} onChange={(event) => setEnteredAmount(event.target.value)}></input>
+
+        <button type="submit">Add</button>
+      </form>
+
+      <div>
+        <div>  <h1>Total amount (Rs): {totalAmount.toFixed(2)}</h1></div>
+        <h2>Products</h2>
+        <ul>
+          {
+            expenses.map((expense, index) => (
+              <li key={index}>
+                Product Name: {expense.title}, Amount: Rs: {expense.amount}
+                <button onClick={() => deleteExpenseHandler(index)}>Delete</button>
+              </li>
+            ))
+          }
+        </ul>
+      </div>
     </div>
   );
-}
-
-export default App;
+};
+ 
+export default App; 
